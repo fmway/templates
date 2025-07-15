dddf1f8207c212a3a4301b37845c0bf5b6a2c853
fmway <fm18lv@gmail.com>
2025-07-15 18:48:00 +0700

    chore: migrations from https://github.com/fmway/lib

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    fmway-lib.url = "github:fmway/lib";
+  };
+
+  outputs = { fmway-lib, self, ... } @ inputs: let
+    inherit (fmway-lib) lib;
+  in {
+    # FIXME use static gen instead of auto import
+    templates = let
+      dir = ./templates;
+      list = with lib; attrNames (
+        filterAttrs (k: _: pathIsRegularFile "${dir}/${k}/flake.nix") (
+          builtins.readDir dir
+        )
+      );
+    in lib.mapListToAttrs (name: {
+      inherit name;
+      value = let
+        path = "${dir}/${name}";
+        description = (import "${path}/flake.nix").description or "";
+      in { inherit path description; };
+    }) list;
+  };
+}
